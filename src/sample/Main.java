2503a56322cc5e08e29c6b90825cdaf1d0776c5e
Nyamur <61559452+Nyamur@users.noreply.github.com>
2021-04-13 15:38:44 +0300

    изменение кнопок состояния

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage primaryStage;
+//    private BorderPane testScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
-        primaryStage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("C:\\Users\\ASUS\\Desktop\\JavaToDo\\src\\sample\\views\\Registration.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("C:\\Users\\ASUS\\Desktop\\JavaToDo\\src\\sample\\views\\Registration.fxml"));
+        AnchorPane testScene = loader.load();
 
+        Scene scene = new Scene(testScene);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("TO DO");
+        primaryStage.show();
 
     }
 
